27b67fb966744b628524ba52a1886e32f2cb71c6
egaadyatma <egaadyatmapuspito@gmail.com>
2025-09-11 09:46:25 +0700

    awalan projek

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EMYU STORE</title>
+</head>
+<body>
+    <h1>EMYU STORE</h1>
+    <p>Untuk kebutuhan game anda 🫵</p>
+</body>
+</html>
