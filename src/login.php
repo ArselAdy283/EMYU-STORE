b31f1412a1d966ef2977756cc2ebecd78336e1f8
egaadyatma <egaadyatmapuspito@gmail.com>
2025-09-15 23:03:06 +0700

    menambahkan login

---
+++ src/login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EMYUSTORE</title>
+    <link href="./output.css" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body class="bg-gradient-to-tr from-[color:#ff392c] via-black to-[color:#ff392c] min-h-screen text-white">
+    <?php
+    include 'navbar.php';
+    ?>
+    
+</body>
+</html>
